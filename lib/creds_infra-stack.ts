6fdcd701011ea631b0494d0b3ea830a6ceac8535
Chad Voegele <chad@voegele.me>
2021-03-23 05:06:34 -0700

    Switch trust order for user and admin role

--- lib/creds_infra-stack.ts
+++ lib/creds_infra-stack.ts
@@ -5,12 +5,6 @@ export class CredsInfraStack extends cdk.Stack {
   constructor (scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
 
-    const admin = new iam.Role(this, 'AdminRole', {
-      assumedBy: new iam.AccountRootPrincipal(),
-      roleName: 'admin',
-      managedPolicies: [iam.ManagedPolicy.fromAwsManagedPolicyName('AdministratorAccess')]
-    })
-
     const userName = process.env.USER
     if (userName === undefined) {
       throw new Error('Could not get USER from evironment!')
@@ -22,8 +16,15 @@ export class CredsInfraStack extends cdk.Stack {
     })
     user.addToPolicy(new iam.PolicyStatement({
       actions: ['sts:AssumeRole'],
-      resources: [admin.roleArn]
+      resources: ['*']
     }))
     new cdk.CfnOutput(this, 'UserArn', { value: user.userArn, exportName: 'UserArn' })
+
+    const admin = new iam.Role(this, 'AdminRole', {
+      assumedBy: user,
+      roleName: 'admin',
+      managedPolicies: [iam.ManagedPolicy.fromAwsManagedPolicyName('AdministratorAccess')]
+    })
+
   }
 }
